4507bfbabcf8a6ba9959856e56ab75a704ff0b47
Deberson Paula <deberson.paula@gmail.com>
2019-07-29 12:42:33 -0300

    added junit and cobertura support for tests

--- config/testUtils/jest.base.js
+++ config/testUtils/jest.base.js
@@ -1,27 +1,28 @@
-module.exports = (source) => ({
+module.exports = source => ({
   testMatch: [`<rootDir>/${source}/**/*.test.{js,jsx,ts,tsx}`],
   transform: JSON.stringify({
     '^.+\\.(ts|tsx)$': eval(`require.resolve('./babelTransform')`),
     '^.+\\.(js|jsx)$': eval(`require.resolve('./babelTransformES6')`),
   }),
-  
+
   clearMocks: true,
   collectCoverage: true,
-  coverageReporters: ['json', 'html', 'lcovonly'],
+  coverageReporters: ['json', 'html', 'lcovonly', 'cobertura'],
   collectCoverageFrom: [`${source}/**/*.{ts,tsx}`, `!${source}/**/*.d.ts`],
   coverageThreshold: JSON.stringify({
     global: {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80
+      statements: 80,
     },
   }),
 
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      eval(`require.resolve('./fileMock')`),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': eval(
+      `require.resolve('./fileMock')`
+    ),
     '\\.(css)$': eval(`require.resolve('./styleMock')`),
   },
   setupFiles: [
@@ -29,5 +30,6 @@ module.exports = (source) => ({
     eval(`require.resolve('./test-shim')`),
   ],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  verbose: true
+  verbose: true,
+  reporters: ['default', ['jest-junit', { "outputDirectory": "./coverage", "outputName": "./junit.xml", }]],
 });
