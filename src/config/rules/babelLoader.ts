ee8de19b420d12165b808bec170183e576c25416
Deberson Paula <deberson.paula@gmail.com>
2020-02-21 08:43:27 -0300

    fixed babelLoader

--- src/config/rules/babelLoader.ts
+++ src/config/rules/babelLoader.ts
@@ -7,13 +7,17 @@ import { babelPlugins } from '../utils/babelPlugins';
  * Process application JS with Babel.
  * The preset includes JSX, Flow, TypeScript, and some ESnext features.
  */
-export const babelLoader = (env: EnvType): RuleSetRule => ({
+export const babelLoader = (
+  env: EnvType,
+  reactHotLoader: boolean
+): RuleSetRule => ({
   test: /\.(ts|tsx|js|mjs|jsx)$/,
   loader: 'babel-loader',
+  exclude: /node_modules/,
   options: {
     babelrc: false,
     configFile: false,
     presets: babelPresets(env),
-    plugins: babelPlugins(env),
+    plugins: babelPlugins(reactHotLoader),
   },
 });
